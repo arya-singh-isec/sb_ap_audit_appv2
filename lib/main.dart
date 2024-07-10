0ee96e1339473d17e24255b7f91e250f6e5d8f04
arya-singh-isec <arya.singh@icicisecurities.com>
2024-07-10 16:08:26 +0530

    added config for edge-to-edge rendering

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,18 @@ import 'core/logger/logger_config.dart';
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   LoggerConfig.initLogger();
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      systemStatusBarContrastEnforced: true,
+      systemNavigationBarColor: Colors.transparent,
+      systemNavigationBarDividerColor: Colors.transparent,
+      systemNavigationBarIconBrightness: Brightness.dark,
+    ),
+  );
+  SystemChrome.setEnabledSystemUIMode(
+    SystemUiMode.edgeToEdge,
+    overlays: [SystemUiOverlay.top],
+  );
   SystemChrome.setPreferredOrientations(
       [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]).then((_) {
     runApp(MyApp());
